56f15d90c3163645d0eba73f03035425096aabfa
jheverdoubling <jinhoyang@everdoubling.ai>
2021-10-01 20:28:15 +0900

    sympy를 이용한 일차방정식 풀이 모듈 추가

--- math_solver.py
+++ math_solver.py
@@ -3,6 +3,9 @@
 # 시간 제한을 둘 수 있도록 필요한 모듈
 import signal
 from contextlib import contextmanager
+from sympy import Symbol, symbols
+import sympy
+import re
 
 class TimeoutException(Exception): pass
 
@@ -19,6 +22,54 @@ def time_limit(seconds):
 
 # %%
 
+# sympy에서 풀 수 있는 형태로 수식 변경
+# ['귤+자몽=42', '귤=자몽-16'] -> ['귤+자몽-(42)', '귤-(자몽+16)']
+def equation_substitution(equations):
+    substitued_equations = []
+    for equation in equations:
+        left_eq, right_eq = equation.split('=')
+        substitued_eq = left_eq + '-(' + right_eq + ')'
+        substitued_equations.append(substitued_eq)
+
+    return substitued_equations
+
+# sympy를 이용한 답 구하기
+def find_answer_using_sympy(substitued_equations):
+    variables = set()
+    for equation in substitued_equations:
+         # 한글 단어들을 변수로 설정
+         vars = re.compile('[가-힣]+').findall(equation)
+         variables.update(vars)
+
+    var_symbol_list = []
+    for var in variables:
+        var_symbol_list.append(Symbol(var))
+
+    # sympy를 이용한 방정식 풀이
+    eq_dict = sympy.solve(substitued_equations, var_symbol_list)
+
+    return eq_dict
+
+def solution_code_generate(equations, eq_dict, objective):
+    answer_str = "vars = dict()\\n"
+    for key, value in eq_dict.items():
+        answer_str+= "vars['" + str(key) + "']" + "=" + str(value) + "\\n"
+    answer_str += "if "
+    for index, equation in enumerate(equations):
+        replaced_str = equation.replace("=","==")
+
+        vars = re.compile('[가-힣]+').findall(replaced_str)
+        for var in vars:
+            replaced_str = replaced_str.replace(var, "vars['" + var + "']")
+        answer_str += replaced_str
+
+        if index != (len(equations)-1):
+            answer_str += " and "
+    answer_str +=":\\n    "
+    answer_str +="print("+ "vars['" + objective + "']" + ")"
+
+    return answer_str
+
 # 주어진 statements(equation, code 등)에서 실행가능한 python 코드를 생성하고, 실행해서 얻어진 답을 반환한다.
 # statements: equation, code, objective
 def do_math(statements):
@@ -27,6 +78,17 @@ def do_math(statements):
 
     if 'equation' in statements:
         pass
+
+    if len(statements['equation']) > 0 and len(statements['objective']) > 0:
+        equations = statements['equation']
+        objective = statements['objective'][0]
+        # 수식을 좌변으로 모음
+        substitued_equations = equation_substitution(equations)
+        # sympy를 이용해서 정답을 찾음
+        eq_dict = find_answer_using_sympy(substitued_equations)
+        # 정답을 기반으로 solution 코드를 생성
+        answer_str = solution_code_generate(equations, eq_dict, objective)
+        print(answer_str)
 
     derivation = statements['code']
     # if 'objective' in statements:
